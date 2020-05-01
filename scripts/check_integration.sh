2daad7f2eb74cfc5b4f94eae240660c926de38a0
Firas Qutishat <firas.qutishat@securekey.com>
2020-05-01 13:57:26 -0400

    chore: update v1 uniresolver image
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- scripts/check_integration.sh
+++ scripts/check_integration.sh
@@ -6,9 +6,38 @@
 #
 set -e
 
-echo "Running vc integration tests..."
+declare -a tests=(
+   "holder_rest"
+   "issuer_rest"
+   "vc_rest"
+)
+
 PWD=`pwd`
 TAGS="${TAGS:all}"
 cd test/bdd
-go test -count=1 -v -cover . -p 1 -timeout=20m -race
+
+totalAgents=${SYSTEM_TOTALJOBSINPHASE:-0}   # standard VSTS variables available using parallel execution; total number of parallel jobs running
+agentNumber=${SYSTEM_JOBPOSITIONINPHASE:-0} # current job position
+testCount=${#tests[@]}
+
+# below conditions are used if parallel pipeline is not used. i.e. pipeline is running with single agent (no parallel configuration)
+if [ "$agentNumber" -eq 0 ]; then agentNumber=1; fi
+
+if [ "$totalAgents" -eq 0 ]; then
+  echo "Running vc integration tests..."
+  go test -count=1 -v -cover . -p 1 -timeout=40m
+else
+  if [ "$agentNumber" -gt $totalAgents ]; then
+    echo "No more tests to run"
+  else
+    for ((i = "$agentNumber"; i <= "$testCount"; )); do
+      testToRun=("${tests[$i - 1]}")
+      echo "***** Running the following test: $testToRun"
+      go test -count=1 -v -cover . -p 1 -timeout=30m -run $testToRun
+      i=$((${i} + ${totalAgents}))
+    done
+    mv ./docker-compose.log "./docker-compose-$agentNumber.log"
+  fi
+fi
+
 cd $PWD
