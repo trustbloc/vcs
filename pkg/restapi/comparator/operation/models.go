2d7b59026fff78df1da3aeeebae8d123ce9dc30f
Firas Qutishat <firas.qutishat@securekey.com>
2021-02-04 09:31:26 -0500

    feat: setup comparator models
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

---
+++ pkg/restapi/comparator/operation/models.go
@@ -0,0 +1,66 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package operation
+
+// Query is an abstract query object.
+type Query struct {
+	// discriminator: true
+	Type string `json:"type"`
+}
+
+// DocQuery is a concrete Query object that identifies a specific document.
+type DocQuery struct {
+	DocID string `json:"docID"`
+	// swagger:allOf Query
+	Query
+	UpstreamAuth struct {
+		EDV string `json:"edv"`
+		KMS string `json:"kms"`
+	} `json:"authTokens"`
+}
+
+// Authorization is given to requesting parties to allow them access to protected resources.
+type Authorization struct {
+	ID              string `json:"id"`
+	RequestingParty string `json:"requestingParty"`
+	Scope           Scope  `json:"scope"`
+	AuthToken       string `json:"authToken"`
+}
+
+// Scope is the scope of the authorized access.
+type Scope struct {
+	DocID   string   `json:"docID"`
+	Actions []string `json:"actions"`
+	Caveats []Caveat `json:"caveats"`
+}
+
+// Caveat are orthogonal, reusable constraints placed on authorization scopes.
+type Caveat struct {
+	Type string `json:"type"`
+}
+
+// ExpiryCaveat is a concrete Caveat that sets a duration on an authorization's scope.
+type ExpiryCaveat struct {
+	Caveat
+	Duration int `json:"duration"`
+}
+
+// Operator is an abstract operation.
+type Operator struct {
+	Type string `json:"type"`
+}
+
+// EqualityOperator is a concrete Operator.
+type EqualityOperator struct {
+	Operator
+	Args []Query `json:"args"`
+}
+
+// Comparison is the result of a comparison.
+type Comparison struct {
+	Result bool `json:"result"`
+}
--- pkg/restapi/comparator/operation/operations_test.go
+++ pkg/restapi/comparator/operation/operations_test.go
@@ -7,6 +7,8 @@ SPDX-License-Identifier: Apache-2.0
 package operation_test
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -16,10 +18,54 @@ import (
 
 func Test_New(t *testing.T) {
 	t.Run("test success", func(t *testing.T) {
-		controller, err := operation.New(nil)
+		op, err := operation.New(nil)
 		require.NoError(t, err)
-		require.NotNil(t, controller)
+		require.NotNil(t, op)
 
-		require.Equal(t, 0, len(controller.GetRESTHandlers()))
+		require.Equal(t, 4, len(op.GetRESTHandlers()))
+	})
+}
+
+func TestOperation_CreateAuthorization(t *testing.T) {
+	t.Run("TODO - creates an authorization", func(t *testing.T) {
+		op, err := operation.New(nil)
+		require.NoError(t, err)
+		require.NotNil(t, op)
+		result := httptest.NewRecorder()
+		op.CreateAuthorization(result, nil)
+		require.Equal(t, http.StatusCreated, result.Code)
+	})
+}
+
+func TestOperation_Compare(t *testing.T) {
+	t.Run("TODO - runs a comparison", func(t *testing.T) {
+		op, err := operation.New(nil)
+		require.NoError(t, err)
+		require.NotNil(t, op)
+		result := httptest.NewRecorder()
+		op.Compare(result, nil)
+		require.Equal(t, http.StatusOK, result.Code)
+	})
+}
+
+func TestOperation_Extract(t *testing.T) {
+	t.Run("TODO - performs an extraction", func(t *testing.T) {
+		op, err := operation.New(nil)
+		require.NoError(t, err)
+		require.NotNil(t, op)
+		result := httptest.NewRecorder()
+		op.Extract(result, nil)
+		require.Equal(t, http.StatusOK, result.Code)
+	})
+}
+
+func TestOperation_Config(t *testing.T) {
+	t.Run("TODO - get config", func(t *testing.T) {
+		op, err := operation.New(nil)
+		require.NoError(t, err)
+		require.NotNil(t, op)
+		result := httptest.NewRecorder()
+		op.Config(result, nil)
+		require.Equal(t, http.StatusOK, result.Code)
 	})
 }
