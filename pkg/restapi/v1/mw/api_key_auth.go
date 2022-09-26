8a7567f9bc0ec1d2c5285b32910e6b86c4e73393
Andrii Holovko <andriy.holovko@gmail.com>
2022-09-26 11:40:58 +0300

    fix: skip health check endpoint in auth middleware
    Closes #806
    
    Signed-off-by: Andrii Holovko <andriy.holovko@gmail.com>

--- pkg/restapi/v1/mw/api_key_auth.go
+++ pkg/restapi/v1/mw/api_key_auth.go
@@ -9,18 +9,24 @@ package mw
 import (
 	"crypto/subtle"
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 )
 
 const (
-	header = "X-API-Key"
+	header          = "X-API-Key"
+	healthCheckPath = "/healthcheck"
 )
 
 // APIKeyAuth returns a middleware that authenticates requests using the API key from X-API-Key header.
 func APIKeyAuth(apiKey string) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
+			if strings.HasSuffix(strings.ToLower(c.Request().URL.Path), healthCheckPath) {
+				return next(c)
+			}
+
 			apiKeyHeader := c.Request().Header.Get(header)
 			if subtle.ConstantTimeCompare([]byte(apiKeyHeader), []byte(apiKey)) != 1 {
 				return &echo.HTTPError{
