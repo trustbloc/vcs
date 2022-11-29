3994cde68203282220a45eae98d091d71103e65a
Stas Dm <iqpirat@gmail.com>
2022-11-29 16:09:39 +0100

    feat: wallet-cli pre-authorize support
    Signed-off-by: Stas D <stanislav.dmytryshyn@securekey.com>

--- component/wallet-cli/cmd/oidc4ci.go
+++ component/wallet-cli/cmd/oidc4ci.go
@@ -7,7 +7,13 @@ SPDX-License-Identifier: Apache-2.0
 package cmd
 
 import (
+	"crypto/tls"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strings"
 
 	"github.com/spf13/cobra"
 
@@ -23,6 +29,7 @@ const (
 type oidc4ciCommandFlags struct {
 	QRCode              string
 	InitiateIssuanceURL string
+	DemoIssuerURL       string
 	ClientID            string
 	GrantType           string
 	Scope               []string
@@ -37,29 +44,19 @@ type oidc4ciCommandFlags struct {
 
 func NewOIDC4CICommand() *cobra.Command {
 	flags := &oidc4ciCommandFlags{}
+	var contextProvider, didDomain, didServiceAuthToken, uniResolverUrl string
 
 	cmd := &cobra.Command{
 		Use:   "oidc4ci",
 		Short: "Request vc with oidc4ci authorized or pre-authorized code flow",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if flags.QRCode == "" && flags.InitiateIssuanceURL == "" {
+			isPreAuthorize := flags.GrantType == preAuthorizedCodeGrantType
+			if flags.QRCode == "" && flags.InitiateIssuanceURL == "" && flags.DemoIssuerURL == "" {
 				return fmt.Errorf("either qr-code or initiate-issuance-url should be set")
 			}
 
 			if flags.GrantType != authorizationCodeGrantType && flags.GrantType != preAuthorizedCodeGrantType {
 				return fmt.Errorf("invalid grant-type")
-			}
-
-			if len(flags.Scope) == 0 {
-				return fmt.Errorf("missing scope")
-			}
-
-			if flags.RedirectURI == "" {
-				return fmt.Errorf("missing redirect-uri")
-			}
-
-			if flags.LoginURL == "" {
-				return fmt.Errorf("missing login-url")
 			}
 
 			if flags.CredentialType == "" {
@@ -70,7 +67,30 @@ func NewOIDC4CICommand() *cobra.Command {
 				return fmt.Errorf("missing credential-format")
 			}
 
+			if !isPreAuthorize {
+				if len(flags.Scope) == 0 {
+					return fmt.Errorf("missing scope")
+				}
+
+				if flags.RedirectURI == "" {
+					return fmt.Errorf("missing redirect-uri")
+				}
+
+				if flags.LoginURL == "" {
+					return fmt.Errorf("missing login-url")
+				}
+			}
+
 			var initiateIssuanceURL string
+
+			if flags.DemoIssuerURL != "" {
+				parsedUrl, err := readIssuanceCodeFromIssuerUrl(flags.DemoIssuerURL)
+				if err != nil {
+					return fmt.Errorf("can not read url from demo issuer %v", err)
+				}
+
+				flags.InitiateIssuanceURL = parsedUrl
+			}
 
 			if flags.InitiateIssuanceURL != "" {
 				initiateIssuanceURL = flags.InitiateIssuanceURL
@@ -87,6 +107,19 @@ func NewOIDC4CICommand() *cobra.Command {
 				func(c *vcprovider.Config) {
 					c.VCFormat = flags.VCFormat
 					c.Debug = flags.Debug
+
+					if contextProvider != "" {
+						c.ContextProviderURL = contextProvider
+					}
+					if didDomain != "" {
+						c.DidDomain = didDomain
+					}
+					if didServiceAuthToken != "" {
+						c.DidServiceAuthToken = didServiceAuthToken
+					}
+					if uniResolverUrl != "" {
+						c.UniResolverURL = uniResolverUrl
+					}
 				},
 			}
 
@@ -106,7 +139,7 @@ func NewOIDC4CICommand() *cobra.Command {
 				Interactive:         false,
 			}
 
-			if flags.GrantType == preAuthorizedCodeGrantType {
+			if isPreAuthorize {
 				return runner.RunOIDC4CIPreAuth(config)
 			}
 
@@ -114,8 +147,14 @@ func NewOIDC4CICommand() *cobra.Command {
 		},
 	}
 
+	cmd.Flags().StringVar(&contextProvider, "context-provider-url", "", "context provider. example: https://static-file-server.stg.trustbloc.dev/ld-contexts.json") //nolint
+	cmd.Flags().StringVar(&didDomain, "did-domain", "", "did domain. example: https://orb-1.stg.trustbloc.dev")                                                     //nolint
+	cmd.Flags().StringVar(&didServiceAuthToken, "did-service-auth-token", "", "did service authorization token. example: tk1")                                      //nolint
+	cmd.Flags().StringVar(&uniResolverUrl, "uni-resolver-url", "", "uni resolver url. example: https://did-resolver.stg.trustbloc.dev/1.0/identifiers")             //nolint
+
 	cmd.Flags().StringVar(&flags.QRCode, "qr-code", "", "path to file with QR code")
 	cmd.Flags().StringVar(&flags.InitiateIssuanceURL, "initiate-issuance-url", "", "initiate issuance url")
+	cmd.Flags().StringVar(&flags.DemoIssuerURL, "demo-issuer-url", "", "demo issuer url. will automatically download qrcode")
 	cmd.Flags().StringVar(&flags.ClientID, "client-id", "", "oauth2 client ID")
 	cmd.Flags().StringVar(&flags.GrantType, "grant-type", "authorization_code", "grant type")
 	cmd.Flags().StringSliceVar(&flags.Scope, "scope", nil, "oauth2 scopes. Can be used to pass credential type")
@@ -128,4 +167,34 @@ func NewOIDC4CICommand() *cobra.Command {
 	cmd.Flags().BoolVar(&flags.Debug, "debug", false, "enable debug mode")
 
 	return cmd
+}
+
+func readIssuanceCodeFromIssuerUrl(issuerUrl string) (string, error) {
+	httpClient := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+	resp, err := httpClient.Get(issuerUrl)
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("invalid status code. expected 200, got %v", resp.StatusCode)
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	_ = resp.Body.Close()
+
+	urlParsed := string(data)
+	r := regexp.MustCompile(`(openid-initiate-issuance://\?[^<]+)`)
+	urlParsed = r.FindString(urlParsed)
+	urlParsed, err = url.QueryUnescape(urlParsed)
+	urlParsed = strings.ReplaceAll(urlParsed, "&amp;", "&")
+
+	return urlParsed, nil
 }
--- component/wallet-cli/pkg/walletrunner/wallet_runner_oidc4ci_pre_auth.go
+++ component/wallet-cli/pkg/walletrunner/wallet_runner_oidc4ci_pre_auth.go
@@ -10,13 +10,14 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"net/url"
-	"regexp"
 	"strings"
-	"time"
+
+	"github.com/hyperledger/aries-framework-go/pkg/wallet"
+	"github.com/samber/lo"
+	"golang.org/x/oauth2"
 
 	"github.com/trustbloc/vcs/pkg/restapi/v1/oidc4ci"
 )
@@ -30,61 +31,30 @@ func (s *Service) RunOIDC4CIPreAuth(config *OIDC4CIConfig) error {
 		},
 	}
 
-	// tood debug
-	resp, err := httpClient.Get("https://demo-issuer.stg.trustbloc.dev/pre-authorize")
-	if err != nil {
-		return err
-	}
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-	resp.Body.Close()
-	r := regexp.MustCompile(`(openid-initiate-issuance://\?[^<]+)`)
-	config.InitiateIssuanceURL = r.FindString(string(data))
-	config.InitiateIssuanceURL, err = url.QueryUnescape(config.InitiateIssuanceURL)
-	config.InitiateIssuanceURL = strings.ReplaceAll(config.InitiateIssuanceURL, "&amp;", "&")
-	if err != nil {
-		return err
-	}
-	// end debug
-
 	parsedUrl, err := url.Parse(config.InitiateIssuanceURL)
 	if err != nil {
 		return fmt.Errorf("failed to parse url %w", err)
 	}
 
-	fmt.Println(parsedUrl.Query())
+	log.Println("Getting issuer OIDC config from well-known endpoint")
+	oidcConfig, err := s.getIssuerOIDCConfig(parsedUrl.Query().Get("issuer"))
 
-	issuerUrl := parsedUrl.Query().Get("issuer")
-	wellKnownUrl, _ := url.JoinPath(issuerUrl, ".well-known/openid-configuration")
-	if err != nil {
-		return fmt.Errorf("failed to create wellKnownUrl %w", err)
-	}
+	tokenEndpoint := oidcConfig.TokenEndpoint
+	credentialsEndpoint := oidcConfig.CredentialEndpoint
 
-	log.Println("Well known url is " + wellKnownUrl)
-
-	resp, err = httpClient.Get(wellKnownUrl)
-	if err != nil {
-		return err
-	}
-
-	var wellKnown map[string]interface{}
-	if err = json.NewDecoder(resp.Body).Decode(&wellKnown); err != nil {
-		return err
-	}
-
-	// todo check if pin required
-	tokenEndpoint := wellKnown["token_endpoint"].(string)
 	log.Println("Token url is" + tokenEndpoint)
+	log.Println("Credentials url is" + credentialsEndpoint)
 
-	tokenResp, tokenErr := httpClient.PostForm(tokenEndpoint, url.Values{
+	tokenValues := url.Values{
 		"grant_type":          []string{"urn:ietf:params:oauth:grant-type:pre-authorized_code"},
 		"pre-authorized_code": []string{parsedUrl.Query().Get("pre-authorized_code")},
-		"user_pin":            []string{"1234"},
-	})
-	//b, _ := io.ReadAll(tokenResp.Body)
-	//fmt.Println(string(b))
+	}
+
+	if strings.EqualFold(parsedUrl.Query().Get("user_pin_required"), "true") {
+		tokenValues.Add("user_pin", "1234") // todo logic for correct pin generation
+	}
+
+	tokenResp, tokenErr := httpClient.PostForm(tokenEndpoint, tokenValues)
 	if tokenErr != nil {
 		return tokenErr
 	}
@@ -95,94 +65,35 @@ func (s *Service) RunOIDC4CIPreAuth(config *OIDC4CIConfig) error {
 	}
 	_ = tokenResp.Body.Close()
 
+	s.oauthClient = &oauth2.Config{ClientID: "oidc4vc_client"} // todo dynamic client registration
+	s.token = lo.ToPtr(oauth2.Token{AccessToken: token.AccessToken}).WithExtra(map[string]interface{}{
+		"c_nonce": *token.CNonce,
+	})
+
 	log.Println("Creating wallet")
 	err = s.CreateWallet()
 	if err != nil {
 		return fmt.Errorf("failed to create wallet: %w", err)
 	}
 
-	proofValue, err := s.createProof(
-		"oidc4vc_client", // todo dynamic client registration
-		*token.CNonce,
-		s.vcProviderConf.WalletParams.DidKeyID,
-	)
+	log.Println("Getting credential")
+	vc, err := s.getCredential(credentialsEndpoint, config.CredentialType, config.CredentialFormat)
 	if err != nil {
-		return err
+		return fmt.Errorf("get credential: %w", err)
 	}
 
-	fmt.Println(proofValue)
+	b, err := json.Marshal(vc)
+	if err != nil {
+		return fmt.Errorf("marshal vc: %w", err)
+	}
+
+	log.Println("Adding credential to wallet")
+	if err = s.wallet.Add(s.vcProviderConf.WalletParams.Token, wallet.Credential, b); err != nil {
+		return fmt.Errorf("add credential to wallet: %w", err)
+	}
+	log.Println("Credentials added successfully")
 
 	s.wallet.Close()
 
 	return nil
-}
-
-func (s *Service) createProof(
-	clientId string,
-	cNonce string,
-	verificationKID string,
-) (string, error) {
-	//jwtHeaders := map[string]interface{}{
-	//	"alg": "EdDSA",
-	//	"kid": verificationKID,
-	//}
-
-	sign, err := signToken(map[string]interface{}{
-		"iss":   clientId,
-		"iat":   time.Now().Unix(),
-		"nonce": cNonce,
-	}, s.vcProviderConf.WalletParams.DidKeyID, s.ariesServices.crypto, s.ariesServices.kms)
-	if err != nil {
-		return "", err
-	}
-
-	return sign, nil
-	//
-	//s.ariesServices.KMS()
-	//ww := s.wallet
-	//fmt.Println(ww)
-	//s.CreateWallet()
-	//keyPair, err := s.wallet.CreateKeyPair(s.vcProviderConf.WalletParams.Token, kms.ED25519)
-	//if err != nil {
-	//	return "", err
-	//}
-	//fmt.Println(keyPair)
-	//keyId, _, err := s.ariesServices.KMS().CreateAndExportPubKeyBytes(kms.ED25519)
-	//if err != nil {
-	//	return "", err
-	//}
-	//
-	//keyHandle, err := s.ariesServices.kms.Get(keyId)
-	//if err != nil {
-	//	return "", err
-	//}
-	//
-	////keyHandle, err := s.ariesServices.kms.Get(keyPair.KeyID)
-	////if err != nil {
-	////	return "", err
-	////}
-	////
-	////jsonToken := jwt.JSONWebToken{
-	////	Headers: jwtHeaders,
-	////	Payload:
-	////}
-	//
-	//payload, err := json.Marshal(map[string]interface{}{
-	//	"iss":   clientId,
-	//	"iat":   time.Now().Unix(),
-	//	"nonce": cNonce,
-	//})
-	//if err != nil {
-	//	return "", err
-	//}
-	//
-	//signed, err := s.ariesServices.crypto.Sign(payload, keyHandle)
-	//fmt.Println(string(signed))
-	//if err != nil {
-	//	return "", err
-	//}
-	//
-	//fmt.Println(string(signed))
-	//
-	//return string(signed), nil
 }
