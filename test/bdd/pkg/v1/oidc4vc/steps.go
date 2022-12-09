300c3d3f5b46c10d8a7a168df324bf73b9bc6ff9
Stas Dm <iqpirat@gmail.com>
2022-12-09 14:09:44 +0100

    fix: bdd
    Signed-off-by: Stas D <stanislav.dmytryshyn@securekey.com>

--- test/bdd/pkg/v1/oidc4vc/steps.go
+++ test/bdd/pkg/v1/oidc4vc/steps.go
@@ -53,6 +53,8 @@ func NewSteps(ctx *bddcontext.BDDContext) (*Steps, error) {
 	walletRunner, err := walletrunner.New(vcprovider.ProviderVCS,
 		func(c *vcprovider.Config) {
 			c.InsecureTls = true
+			c.DidKeyType = "ECDSAP384TypeDER"
+			c.DidMethod = "orb"
 		})
 	if err != nil {
 		return nil, fmt.Errorf("unable create wallet runner: %w", err)
