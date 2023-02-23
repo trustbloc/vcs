d17e9a85e9315fbe243125515dfdab478e6e99ee
Stas Dm <iqpirat@gmail.com>
2023-02-23 22:23:35 +0100

    fix: compose
    Signed-off-by: Stas D <stanislav.dmytryshyn@securekey.com>

--- test/bdd/bddtests_test.go
+++ test/bdd/bddtests_test.go
@@ -81,12 +81,12 @@ func runBDDTests(tags, format string) int {
 }
 
 func InitializeTestSuite(ctx *godog.TestSuiteContext) {
-	//if os.Getenv("DISABLE_COMPOSITION") == "true" {
-	//	return
-	//}
-	//
-	//ctx.BeforeSuite(beforeSuiteHook)
-	//ctx.AfterSuite(afterSuiteHook)
+	if os.Getenv("DISABLE_COMPOSITION") == "true" {
+		return
+	}
+
+	ctx.BeforeSuite(beforeSuiteHook)
+	ctx.AfterSuite(afterSuiteHook)
 }
 
 func beforeSuiteHook() {
