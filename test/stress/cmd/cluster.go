b28d4e22ac27e9619efd164207b8737e32cfad68
Stas D <iqpirat@gmail.com>
2023-05-10 10:35:34 +0200

    feat: basic cluster config
    Signed-off-by: Stas D <stanislav.dmytryshyn@securekey.com>

---
+++ test/stress/cmd/cluster.go
@@ -0,0 +1,51 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func getNodeName() string {
+	hostName, _ := os.Hostname()
+	if v := os.Getenv("CUSTOM_HOST"); v != "" {
+		hostName = v
+	}
+
+	return hostName
+}
+
+func getClusterMembers(ctx context.Context, rdb redis.UniversalClient) (map[string]string, error) {
+	resp := rdb.HGetAll(ctx, orchestratorKey)
+	if resp.Err() != nil {
+		return nil, resp.Err()
+	}
+
+	result, err := resp.Result()
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func getClusterResultKey(id string) string {
+	return fmt.Sprintf("stress:cluster:result:%v", id)
+}
+
+func getResultKey(id string) string {
+	return fmt.Sprintf("stress:result:%v", id)
+}
+
+type clusterResult struct {
+	Id    string            `json:"id"`
+	Nodes map[string]string `json:"nodes"`
+}
--- test/stress/cmd/main.go
+++ test/stress/cmd/main.go
@@ -1,49 +1,162 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
 package main
 
 import (
 	"context"
 	"crypto/tls"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
-	"github.com/bluele/gcache"
+	"github.com/imroc/req/v3"
+
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
+	"github.com/redis/go-redis/v9"
 	"github.com/trustbloc/logutil-go/pkg/log"
 
 	"github.com/trustbloc/vcs/test/stress/pkg/stress"
 )
 
-var results = gcache.New(3).LRU().Build()
 var logger = log.New("stress-test-cmd")
+
+const (
+	orchestratorKey = "stress:nodes"
+)
 
 func main() {
 	e := echo.New()
 	hostName, _ := os.Hostname()
+	apiAddress := os.Getenv("API_ADDRESS")
+	k8SvcName := os.Getenv("K8_HEADLESS_SVC")
+	port := strings.Split(apiAddress, ":")[1]
+
+	rdb := redis.NewUniversalClient(&redis.UniversalOptions{
+		Addrs:    []string{"localhost:6379"},
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	go func() {
+		for context.Background().Err() == nil {
+			localName := hostName
+			if v := os.Getenv("CUSTOM_HOST"); v != "" {
+				localName = v
+			}
+
+			var b strings.Builder
+			b.WriteString(fmt.Sprintf("http://%v", localName))
+			if k8SvcName != "" {
+				b.WriteString(fmt.Sprintf(".%v", k8SvcName))
+			}
+			b.WriteString(fmt.Sprintf(":%v", port))
+			if err := rdb.HSet(context.Background(), orchestratorKey, localName, b.String()).Err(); err != nil {
+				panic(err)
+			}
+
+			logger.Info(fmt.Sprintf("node registered in cluster mode with url: %v", b.String()))
+
+			time.Sleep(30 * time.Second)
+		}
+	}()
+
+	e.GET("/ready", func(c echo.Context) error {
+		return c.String(http.StatusOK, "ok")
+	})
+
+	e.GET("/nodes", func(c echo.Context) error {
+		members, err := getClusterMembers(c.Request().Context(), rdb)
+		if err != nil {
+			return err
+		}
+
+		return c.JSON(http.StatusOK, members)
+	})
 
 	e.POST("/run", func(c echo.Context) error {
 		var cfg request
 
 		c.Response().Header().Set("node", hostName)
-
 		err := json.NewDecoder(c.Request().Body).Decode(&cfg)
 		if err != nil {
 			return err
 		}
 
-		id := uuid.NewString()
+		clusterMembers, err := getClusterMembers(c.Request().Context(), rdb)
+		if err != nil {
+			return err
+		}
 
-		if cfg.ID != "" {
-			v, _ := results.Get(cfg.ID)
+		cfg.ID = uuid.NewString()
+		healthyMembers := map[string]string{}
+		for member, url := range clusterMembers {
+			_, hError := req.Get(url)
+			if hError != nil {
+				logger.Error(fmt.Sprintf("error checking health for node [%v] and url [%v] with error [%v]",
+					member, url, hError))
 
-			if v != nil {
-				return c.String(http.StatusConflict, "job already running on this node")
+				_ = rdb.HDel(c.Request().Context(), orchestratorKey, member)
+				continue
 			}
 
-			id = cfg.ID
+			healthyMembers[member] = url
+		}
+
+		if len(healthyMembers) == 0 {
+			return errors.New("no healthy cluster members")
+		}
+
+		cfg.ConcurrentRequests /= len(healthyMembers)
+
+		res := &clusterResult{
+			Id:    cfg.ID,
+			Nodes: map[string]string{},
+		}
+
+		for member, url := range healthyMembers {
+			var resultId string
+			resp, err := req.R().SetBodyJsonMarshal(cfg).
+				Post(fmt.Sprintf("%v/internal/run", url))
+
+			if err != nil {
+				return fmt.Errorf("can not schedule job on %v with error %w", member, err)
+			}
+
+			if resp.StatusCode != http.StatusOK {
+				return fmt.Errorf("can not schedule job on %v with status code %v", member, resp.StatusCode)
+			}
+
+			resultId = resp.String()
+			res.Nodes[member] = resultId
+		}
+
+		b, err := json.Marshal(res)
+		if err != nil {
+			return err
+		}
+
+		rdb.Set(c.Request().Context(), getClusterResultKey(res.Id), b, 3*time.Hour)
+
+		return c.String(200, res.Id)
+	})
+
+	e.POST("/internal/run", func(c echo.Context) error {
+		var cfg request
+		resultId := uuid.NewString()
+		clusterResultKey := getResultKey(resultId)
+
+		err := json.NewDecoder(c.Request().Body).Decode(&cfg)
+		if err != nil {
+			return err
 		}
 
 		cfg.TLSConfig = &tls.Config{
@@ -57,8 +170,8 @@ func main() {
 				HostName:  hostName,
 			}
 
-			_ = results.Set(id, testRunResult)
-
+			b, _ := json.Marshal(testRunResult)
+			rdb.Set(context.Background(), clusterResultKey, b, 3*time.Hour)
 			res, err2 := stress.NewStressRun(&cfg.Config).Run(context.Background())
 
 			now := time.Now().UTC()
@@ -71,7 +184,7 @@ func main() {
 
 			if err2 != nil {
 				logger.Error(fmt.Sprintf("got error %v for run id %v",
-					err2, id))
+					err2, resultId))
 
 				testRunResult.Errors = []string{fmt.Sprintf("%+v", err2)}
 				testRunResult.State = "failed"
@@ -99,15 +212,18 @@ func main() {
 					})
 				}
 			}
+
+			b, _ = json.Marshal(testRunResult)
+			rdb.Set(context.Background(), clusterResultKey, b, 3*time.Hour)
 		}()
 
-		return c.String(200, id)
+		return c.String(http.StatusOK, clusterResultKey)
 	})
 
 	e.GET("/run/:id", func(c echo.Context) error {
-		item, _ := results.Get(c.Param("id"))
+		item := rdb.Get(c.Request().Context(), getClusterResultKey(c.Param("id")))
 
-		return c.JSON(200, item)
+		return c.String(200, item.String())
 	})
 
 	if errStart := e.Start(os.Getenv("API_ADDRESS")); errStart != nil {
