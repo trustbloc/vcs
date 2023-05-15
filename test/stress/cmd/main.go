13ad65aff2c6ddbb9c1c221478df4c2ba80cd58b
Stas Dmytryshyn <iqpirat@gmail.com>
2023-05-15 20:01:25 +0200

    feat: cluster mode support for stress testing (#1320)
    * feat: basic cluster config
    
    Signed-off-by: Stas D <stanislav.dmytryshyn@securekey.com>
    
    * feat: more work on cluster
    
    Signed-off-by: Stas D <stanislav.dmytryshyn@securekey.com>
    
    * chore: add is finished
    
    Signed-off-by: Stas D <stanislav.dmytryshyn@securekey.com>
    
    * chore: move total rate
    
    Signed-off-by: Stas D <stanislav.dmytryshyn@securekey.com>
    
    * chore: ignore test
    
    Signed-off-by: Stas D <stanislav.dmytryshyn@securekey.com>
    
    * fix: mod tidy
    
    Signed-off-by: Stas D <stanislav.dmytryshyn@securekey.com>
    
    * chore: skip err
    
    Signed-off-by: Stas D <stanislav.dmytryshyn@securekey.com>
    
    ---------
    
    Signed-off-by: Stas D <stanislav.dmytryshyn@securekey.com>

--- test/stress/cmd/main.go
+++ test/stress/cmd/main.go
@@ -1,49 +1,166 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
 package main
 
 import (
 	"context"
 	"crypto/tls"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
-	"github.com/bluele/gcache"
+	"github.com/imroc/req/v3"
+	"github.com/joho/godotenv"
+	"github.com/samber/lo"
+
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
+	"github.com/redis/go-redis/v9"
 	"github.com/trustbloc/logutil-go/pkg/log"
 
 	"github.com/trustbloc/vcs/test/stress/pkg/stress"
 )
 
-var results = gcache.New(3).LRU().Build()
 var logger = log.New("stress-test-cmd")
 
+const (
+	orchestratorKey = "stress:nodes"
+)
+
 func main() {
+	_ = godotenv.Load(".env")
 	e := echo.New()
-	hostName, _ := os.Hostname()
+	hostName := getNodeName()
+	apiAddress := os.Getenv("API_ADDRESS")
+	k8SvcName := os.Getenv("K8_HEADLESS_SVC")
+	port := strings.Split(apiAddress, ":")[1]
+
+	rdb := redis.NewUniversalClient(&redis.UniversalOptions{
+		Addrs:    strings.Split(os.Getenv("REDIS_URL"), ","),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       0,
+	})
+
+	if redisErr := rdb.Ping(context.TODO()).Err(); redisErr != nil {
+		panic(redisErr)
+	}
+
+	go func() {
+		for context.Background().Err() == nil {
+			var b strings.Builder
+			b.WriteString(fmt.Sprintf("http://%v", hostName))
+			if k8SvcName != "" {
+				b.WriteString(fmt.Sprintf(".%v", k8SvcName))
+			}
+			b.WriteString(fmt.Sprintf(":%v", port))
+			if err := rdb.HSet(context.Background(), orchestratorKey, hostName, b.String()).Err(); err != nil {
+				panic(err)
+			}
+
+			logger.Info(fmt.Sprintf("node registered in cluster mode with url: %v", b.String()))
+
+			time.Sleep(30 * time.Second)
+		}
+	}()
+
+	e.GET("/ready", func(c echo.Context) error {
+		return c.String(http.StatusOK, "ok")
+	})
+
+	e.GET("/nodes", func(c echo.Context) error {
+		members, err := getClusterMembers(c.Request().Context(), rdb)
+		if err != nil {
+			return c.String(http.StatusBadRequest, err.Error())
+		}
+
+		return c.JSON(http.StatusOK, members)
+	})
 
 	e.POST("/run", func(c echo.Context) error {
 		var cfg request
 
 		c.Response().Header().Set("node", hostName)
+		err := json.NewDecoder(c.Request().Body).Decode(&cfg)
+		if err != nil {
+			return c.String(http.StatusBadRequest, err.Error())
+		}
+
+		clusterMembers, err := getClusterMembers(c.Request().Context(), rdb)
+		if err != nil {
+			return c.String(http.StatusBadRequest, err.Error())
+		}
+
+		cfg.ID = uuid.NewString()
+		healthyMembers := map[string]string{}
+		for member, url := range clusterMembers {
+			_, hError := req.Get(url)
+			if hError != nil {
+				logger.Error(fmt.Sprintf("error checking health for node [%v] and url [%v] with error [%v]",
+					member, url, hError))
+
+				_ = rdb.HDel(c.Request().Context(), orchestratorKey, member)
+				continue
+			}
+
+			healthyMembers[member] = url
+		}
+
+		if len(healthyMembers) == 0 {
+			return c.String(http.StatusBadRequest, errors.New("no healthy cluster members").Error())
+		}
+
+		cfg.ConcurrentRequests /= len(healthyMembers)
+
+		res := &clusterResult{
+			Id:        cfg.ID,
+			Nodes:     map[string]string{},
+			TotalRate: cfg.ConcurrentRequests * len(healthyMembers),
+		}
+
+		for member, url := range healthyMembers {
+			var resultId string
+			resp, err := req.R().SetBodyJsonMarshal(cfg).
+				Post(fmt.Sprintf("%v/internal/run", url))
+
+			if err != nil {
+				return fmt.Errorf("can not schedule job on %v with error %w", member, err)
+			}
+
+			if resp.StatusCode != http.StatusOK {
+				return fmt.Errorf("can not schedule job on %v with status code %v", member, resp.StatusCode)
+			}
+
+			resultId = resp.String()
+			res.Nodes[member] = resultId
+			time.Sleep(2 * time.Second)
+		}
+
+		b, err := json.Marshal(res)
+		if err != nil {
+			return c.String(http.StatusBadRequest, err.Error())
+		}
+
+		rdb.Set(c.Request().Context(), getClusterResultKey(res.Id), b, 3*time.Hour)
+
+		return c.JSON(200, res)
+	})
+
+	e.POST("/internal/run", func(c echo.Context) error {
+		var cfg request
+		resultId := uuid.NewString()
+		clusterResultKey := getResultKey(resultId)
 
 		err := json.NewDecoder(c.Request().Body).Decode(&cfg)
 		if err != nil {
-			return err
-		}
-
-		id := uuid.NewString()
-
-		if cfg.ID != "" {
-			v, _ := results.Get(cfg.ID)
-
-			if v != nil {
-				return c.String(http.StatusConflict, "job already running on this node")
-			}
-
-			id = cfg.ID
+			return c.String(http.StatusBadRequest, err.Error())
 		}
 
 		cfg.TLSConfig = &tls.Config{
@@ -57,21 +174,21 @@ func main() {
 				HostName:  hostName,
 			}
 
-			_ = results.Set(id, testRunResult)
-
+			b, _ := json.Marshal(testRunResult)
+			rdb.Set(context.Background(), clusterResultKey, b, 3*time.Hour)
 			res, err2 := stress.NewStressRun(&cfg.Config).Run(context.Background())
 
 			now := time.Now().UTC()
 			testRunResult.Result = res
 			testRunResult.FinishedAt = &now
 
-			if testRunResult.Result != nil && !cfg.Detailed {
-				testRunResult.PerCredentialData = nil
-			}
+			//if testRunResult.Result != nil && !cfg.Detailed {
+			//	testRunResult.PerCredentialData = nil
+			//}
 
 			if err2 != nil {
 				logger.Error(fmt.Sprintf("got error %v for run id %v",
-					err2, id))
+					err2, resultId))
 
 				testRunResult.Errors = []string{fmt.Sprintf("%+v", err2)}
 				testRunResult.State = "failed"
@@ -99,15 +216,115 @@ func main() {
 					})
 				}
 			}
+
+			b, _ = json.Marshal(testRunResult)
+			rdb.Set(context.Background(), clusterResultKey, b, 3*time.Hour)
 		}()
 
-		return c.String(200, id)
+		return c.String(http.StatusOK, clusterResultKey)
 	})
 
 	e.GET("/run/:id", func(c echo.Context) error {
-		item, _ := results.Get(c.Param("id"))
+		item := rdb.Get(c.Request().Context(), getClusterResultKey(c.Param("id")))
+		var res clusterResult
 
-		return c.JSON(200, item)
+		b, err := item.Bytes()
+		if err != nil {
+			return c.String(http.StatusBadRequest, err.Error())
+		}
+
+		if err = json.Unmarshal(b, &res); err != nil {
+			return c.String(http.StatusBadRequest, err.Error())
+		}
+
+		finalResult := &combinedResult{
+			IsFinished:      true,
+			TotalNodes:      len(res.Nodes),
+			PerRunnerInfo:   map[string]*perRunnerInfo{},
+			CombinedMetrics: map[string]metric{},
+			GroupedErrors:   map[string]int{},
+			TotalRate:       res.TotalRate,
+		}
+
+		meticData := map[string]*stress.Metric{}
+
+		for id, resultId := range res.Nodes {
+			runnerInfo := &perRunnerInfo{}
+			finalResult.PerRunnerInfo[id] = runnerInfo
+
+			runnerResult := rdb.Get(c.Request().Context(), resultId)
+			if runnerResult.Err() != nil {
+				runnerInfo.ResultError = lo.ToPtr(runnerResult.Err().Error())
+				finalResult.IsFinished = false
+				continue
+			}
+
+			var run runResult
+			rb, err := runnerResult.Bytes()
+			if err != nil {
+				runnerInfo.ResultError = lo.ToPtr(runnerResult.Err().Error())
+				finalResult.IsFinished = false
+				continue
+			}
+
+			if err := json.Unmarshal(rb, &run); err != nil {
+				runnerInfo.ResultError = lo.ToPtr(fmt.Errorf("can not unmarshal. %w", err).Error())
+				finalResult.IsFinished = false
+				continue
+			}
+
+			runnerInfo.State = run.State
+			if runnerInfo.State == "running" {
+				finalResult.IsFinished = false
+			}
+
+			runnerInfo.ErrorCount = run.ErrorsCount
+			finalResult.TotalErrors += run.ErrorsCount
+
+			if run.Result != nil {
+				runnerInfo.RawMetrics = run.Result.Metrics
+
+				runnerInfo.Rate = run.Result.ConcurrentRequests
+
+				runnerInfo.RequestCount = run.Result.UserCount
+				finalResult.TotalRequests += run.Result.UserCount
+
+				if len(run.Result.Metrics) > 0 {
+					for _, m := range run.Result.Metrics {
+						v, ok := meticData[m.Name]
+						if !ok {
+							cp := *m
+							meticData[m.Name] = &cp
+							continue
+						}
+
+						v.Avg = (v.Avg + m.Avg) / 2
+						if v.Min > m.Min {
+							v.Min = m.Min
+						}
+						if v.Max < m.Max {
+							v.Max = m.Max
+						}
+					}
+				}
+
+				if len(run.Errors) > 0 {
+					for _, errStr := range run.Errors {
+						finalResult.GroupedErrors[errStr] += 1
+					}
+				}
+			}
+		}
+
+		for k, v := range meticData {
+			finalResult.CombinedMetrics[k] = metric{
+				Avg: v.Avg.String(),
+				Max: v.Max.String(),
+				Min: v.Min.String(),
+			}
+		}
+
+		return c.JSON(200, finalResult)
 	})
 
 	if errStart := e.Start(os.Getenv("API_ADDRESS")); errStart != nil {
--- test/stress/go.mod
+++ test/stress/go.mod
@@ -7,11 +7,14 @@ module github.com/trustbloc/vcs/test/stress
 go 1.19
 
 require (
-	github.com/bluele/gcache v0.0.2
 	github.com/google/uuid v1.3.0
 	github.com/greenpau/go-calculator v1.0.1
 	github.com/hyperledger/aries-framework-go v0.3.2
+	github.com/imroc/req/v3 v3.34.0
+	github.com/joho/godotenv v1.4.0
 	github.com/labstack/echo/v4 v4.9.0
+	github.com/redis/go-redis/v9 v9.0.3
+	github.com/samber/lo v1.37.0
 	github.com/trustbloc/logutil-go v1.0.0-rc1
 	github.com/trustbloc/vcs/component/wallet-cli v0.0.0-20230314165048-d06b1132a27b
 	github.com/trustbloc/vcs/test/bdd v0.0.0-20230314165048-d06b1132a27b
@@ -37,6 +40,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sts v1.17.6 // indirect
 	github.com/aws/smithy-go v1.13.5 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/bluele/gcache v0.0.2 // indirect
 	github.com/btcsuite/btcd v0.22.1 // indirect
 	github.com/btcsuite/btcutil v1.0.3-0.20201208143702-a53e38424cce // indirect
 	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
@@ -46,6 +50,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/deepmap/oapi-codegen v1.11.0 // indirect
 	github.com/dgraph-io/ristretto v0.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/ecordell/optgen v0.0.6 // indirect
 	github.com/evanphx/json-patch v4.11.0+incompatible // indirect
@@ -56,16 +61,20 @@ require (
 	github.com/go-jose/go-jose/v3 v3.0.1-0.20221117193127-916db76e8214 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/swag v0.21.1 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
 	github.com/golang/glog v1.0.0 // indirect
 	github.com/golang/mock v1.6.0 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/certificate-transparency-go v1.1.2-0.20210512142713-bed466244fa6 // indirect
+	github.com/google/pprof v0.0.0-20230426061923-93006964c1fc // indirect
 	github.com/google/tink/go v1.7.0 // indirect
 	github.com/google/trillian v1.3.14-0.20210520152752-ceda464a95a3 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/henvic/httpretty v0.1.0 // indirect
@@ -108,6 +117,7 @@ require (
 	github.com/multiformats/go-multibase v0.1.1 // indirect
 	github.com/multiformats/go-multihash v0.0.14 // indirect
 	github.com/multiformats/go-varint v0.0.6 // indirect
+	github.com/onsi/ginkgo/v2 v2.9.2 // indirect
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/ory/fosite v0.43.0 // indirect
 	github.com/ory/go-acc v0.2.8 // indirect
@@ -124,7 +134,10 @@ require (
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.32.1 // indirect
 	github.com/prometheus/procfs v0.6.0 // indirect
-	github.com/samber/lo v1.37.0 // indirect
+	github.com/quic-go/qpack v0.4.0 // indirect
+	github.com/quic-go/qtls-go1-19 v0.3.2 // indirect
+	github.com/quic-go/qtls-go1-20 v0.2.2 // indirect
+	github.com/quic-go/quic-go v0.34.0 // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
 	github.com/spf13/afero v1.9.2 // indirect
@@ -163,14 +176,14 @@ require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.23.0 // indirect
-	golang.org/x/crypto v0.1.0 // indirect
-	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
-	golang.org/x/mod v0.8.0 // indirect
-	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/crypto v0.8.0 // indirect
+	golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53 // indirect
+	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
-	golang.org/x/tools v0.6.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/tools v0.8.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20220222213610-43724f9ea8cf // indirect
 	google.golang.org/grpc v1.46.2 // indirect
