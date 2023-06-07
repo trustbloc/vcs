419fa85d852599a8ddaaa116a7ea7d02c6b52417
Stas D <iqpirat@gmail.com>
2023-06-07 14:52:05 +0200

    feat: support more redirect codes for cogntio consent
    Signed-off-by: Stas D <stanislav.dmytryshyn@securekey.com>

--- component/wallet-cli/pkg/walletrunner/consent/cognito_test.go
+++ component/wallet-cli/pkg/walletrunner/consent/cognito_test.go
@@ -8,6 +8,7 @@ package consent_test
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"strings"
@@ -21,6 +22,63 @@ import (
 
 func TestCognitoConsent(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
+		statusCodes := []int{http.StatusMovedPermanently, http.StatusFound, http.StatusSeeOther}
+		for _, code := range statusCodes {
+			t.Run(fmt.Sprintf("verify with status code %v", code), func(t *testing.T) {
+				cl := NewMockhttpClient(gomock.NewController(t))
+
+				targetURL := "https://example.auth.us-east-2.amazoncognito.com/login?client_id=example&redirect_uri=" +
+					"https%3A%2F%2Fexample-redirect.com%2Fvcs%2Foidc%2Fredirect&response_type=code&" +
+					"state=9bc93ec1-7bdd-4084-8948-299ef35adab8"
+
+				ct := consent.NewCognito(
+					cl,
+					[]*http.Cookie{
+						{
+							Name:  "XSRF-TOKEN",
+							Value: "abcd",
+						},
+					},
+					targetURL,
+					"some-login",
+					"some-password",
+				)
+
+				cl.EXPECT().Do(gomock.Any()).DoAndReturn(func(request *http.Request) (*http.Response, error) {
+					assert.Equal(t, http.MethodGet, request.Method)
+					assert.Equal(t, targetURL, request.URL.String())
+
+					return &http.Response{
+						Header: map[string][]string{
+							"Set-Cookie": {
+								"XSRF-TOKEN=8f6cafbe-34c3-4c96-b53b-47c798297e79; Path=/; Secure; HttpOnly; SameSite=Lax",
+							},
+						},
+					}, nil
+				})
+
+				cl.EXPECT().Do(gomock.Any()).DoAndReturn(func(request *http.Request) (*http.Response, error) {
+					assert.Equal(t, http.MethodPost, request.Method)
+					assert.Equal(t, targetURL, request.URL.String())
+					assert.Equal(t, "XSRF-TOKEN", request.Cookies()[0].Name)
+					assert.Equal(t, "8f6cafbe-34c3-4c96-b53b-47c798297e79", request.Cookies()[0].Value)
+					assert.NoError(t, request.ParseForm())
+
+					assert.Equal(t, "some-login", request.Form.Get("username"))
+					assert.Equal(t, "Sign in", request.Form.Get("signInSubmitButton"))
+					assert.Equal(t, "some-password", request.Form.Get("password"))
+					assert.Equal(t, "8f6cafbe-34c3-4c96-b53b-47c798297e79", request.Form.Get("_csrf"))
+					return &http.Response{
+						StatusCode: code,
+					}, nil
+				})
+
+				assert.NoError(t, ct.Execute())
+			})
+		}
+	})
+
+	t.Run("success http 303", func(t *testing.T) {
 		cl := NewMockhttpClient(gomock.NewController(t))
 
 		targetURL := "https://example.auth.us-east-2.amazoncognito.com/login?client_id=example&redirect_uri=" +
@@ -65,7 +123,7 @@ func TestCognitoConsent(t *testing.T) {
 			assert.Equal(t, "some-password", request.Form.Get("password"))
 			assert.Equal(t, "8f6cafbe-34c3-4c96-b53b-47c798297e79", request.Form.Get("_csrf"))
 			return &http.Response{
-				StatusCode: http.StatusFound,
+				StatusCode: http.StatusSeeOther,
 			}, nil
 		})
 
