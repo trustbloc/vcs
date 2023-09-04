a5988c17758ed1f63d2a59afe757d733cb9e384a
Stas Dm <iqpirat@gmail.com>
2023-09-04 15:45:33 +0200

    fix: properly close wallet

--- test/bdd/pkg/v1/oidc4vp/wallet.go
+++ test/bdd/pkg/v1/oidc4vp/wallet.go
@@ -8,6 +8,7 @@ package oidc4vp
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/trustbloc/vcs/component/wallet-cli/pkg/walletrunner"
 	"github.com/trustbloc/vcs/component/wallet-cli/pkg/walletrunner/vcprovider"
@@ -15,23 +16,35 @@ import (
 
 func (e *Steps) createWallet(numOfDIDs int) error {
 	var err error
+	if e.walletRunner != nil {
+		if wallet := e.walletRunner.GetWallet(); wallet != nil {
+			_ = wallet.Close()
+		}
+	}
+
+	e.walletRunner = nil
 	e.walletRunner, err = walletrunner.New(vcprovider.ProviderVCS, func(c *vcprovider.Config) {
 		c.WalletDidCount = numOfDIDs
 		c.DidMethod = "orb"
 	})
+
 	if err != nil {
+		e.walletRunner = nil
 		return fmt.Errorf("walletrunner.New: %w", err)
 	}
 
 	err = e.walletRunner.CreateWallet()
 	if err != nil {
+		e.walletRunner = nil
 		return fmt.Errorf("walletRunner.CreateWallet: %w", err)
 	}
 
-	e.bddContext.CredentialSubject = append(e.bddContext.CredentialSubject, e.walletRunner.GetConfig().WalletParams.DidID...)
+	time.Sleep(1 * time.Second)
+	e.bddContext.CredentialSubject = append(e.bddContext.CredentialSubject,
+		e.walletRunner.GetConfig().WalletParams.DidID...)
+
 	return nil
 }
-
 func (e *Steps) saveCredentialsInWallet() error {
 	for _, cred := range e.bddContext.CreatedCredentialsSet {
 		err := e.walletRunner.SaveCredentialInWallet(cred)
