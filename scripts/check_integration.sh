333412047134d5aa3b00dcc85dd056e88e124dbb
Rolson Quadras <rolson.quadras@gendigital.com>
2023-09-14 10:24:25 -0400

    chore: remove unused orb/sidetree docker container
    Signed-off-by: Rolson Quadras <rolson.quadras@gendigital.com>

--- scripts/check_integration.sh
+++ scripts/check_integration.sh
@@ -12,11 +12,6 @@ TAGS=${TAGS:-all}
 PWD=`pwd`
 cd test/bdd
 
-echo "---
-Important: for these test to run correctly, you must ensure that your hosts file has the following entries:
-127.0.0.1 testnet.orb.local
----
-"
 echo "Running vcs integration tests with tag=$TAGS"
 
 
